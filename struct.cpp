a3c1339c6fa82401e992487440a8291170fc56a5
dzakyakbar887 <dzakyakbar392@gmail.com>
2026-04-07 18:32:31 +0700

    langkah 15

--- struct.cpp
+++ struct.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 using namespace std;
 
-struct out_of_range{
+struct Orang
+{
     string nama;
     string alamat;
     int umur;
 };
 
+int main()
+{
+    //Deklarasi variabel struct
+    Orang mhs;
+    //Mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << "Isikan data umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    cout << "Data tampil" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
+}
